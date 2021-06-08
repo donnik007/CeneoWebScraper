5b81d521c6879ffbe8f363c4b5be68c8d806479b
donnik007 <s218161@student.uek.krakow.pl>
2021-06-08 18:10:25 +0200

    2021-06-08 DS

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from app.models.product import Product
 from flask import render_template, redirect, url_for, request
 from os import listdir
+import os
+import json
 
 
 @app.route('/')
@@ -27,6 +29,29 @@ def extract():
 @app.route('/products')
 def products():
     products = [product.split('.')[0] for product in listdir("app/products")]
+    opinions = []
+    cons = []
+    pros = []
+    avr = []
+    for product in products:
+        with open(f"app/products/{product}.json", "r", encoding="UTF-8") as f:
+            producto = json.load(f)
+            opinions.append(len(producto["opinions"]))
+            sumCons = 0
+            sumPros = 0
+            sumStar = 0
+            for opinion in producto["opinions"]:
+                sumCons += len(opinion["cons"])
+                sumPros += len(opinion["pros"])
+                sumStar += opinion["stars"]
+                
+            cons.append(sumCons)
+            pros.append(sumPros)
+            avr.append(round(sumStar/len(producto["opinions"]),1))
+
+    products = [[products[i], opinions[i], cons[i], pros[i], avr[i]] for i in range(len(products))]
+
+
     return render_template('products.html.jinja',products=products)
 
 @app.route('/about')
