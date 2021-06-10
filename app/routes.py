a412c1c6f7093b78ed879ca49cc42ec3f54090eb
donnik007 <s218161@student.uek.krakow.pl>
2021-06-11 00:41:34 +0200

    2021.06.11 DS dodat

--- app/routes.py
+++ app/routes.py
@@ -63,48 +63,39 @@ def about():
 def product(productId):
     product = Product(productId)
     product.importProduct()
-    return  render_template('product.html.jinja',product=str(product), productName=product.productName)
+    
+    return  render_template('product.html.jinja',product=product.toDict(), productName=product.productName, productId = product.productId)
 
 @app.route('/getCSV/<productId>')
 def getCSV(productId):
-    with open(f"app/products/{productId}.json", "r", encoding="UTF-8") as f:
-        csvf = json.load(f)
-    opinions = csvf['opinions']
+    inputFile = open(f"app/products/{productId}.json", "r", encoding="UTF-8") #open json file
+    outputFile = open("app/static/opinions.csv", 'w', encoding="UTF-8") #load csv file
+    data = json.load(inputFile) #load json content
+    inputFile.close() #close the input file
+    output = csv.writer(outputFile) #create a csv.write
+    output.writerow(data["opinions"][0].keys())  # header row
+    for row in data["opinions"]:
+        output.writerow(row.values()) #values row
 
-    fe = csv.writer(open("opinions.csv", "w"))
-    for x in csvf:
-        fe.writerow([x["opinionId"],
-            x["author"],
-            x["rcmd"],
-            x["stars"],
-            x["content"],
-            x["pros"],
-            x["cons"],
-            x["purchased"],
-            x["publishDate"],
-            x["purchaseDate"],
-            x["useful"],
-            x["useless"]])
-
-    path = "C:\\Users\\domin\\Desktop\\Studia\\2SEM\prac_prog2\\CeneoWebScraper\\opinions.csv"
+    path = "static/opinions.csv"
     return send_file(path, as_attachment=True)
 
 @app.route('/getJSON/<productId>')
 def getJSON(productId):
-    with open(f"app/products/{productId}.json", "r", encoding="UTF-8") as f:
-        producto = json.load(f)
-    return Response(str(producto), mimetype="text/json", headers={"Content-disposition":"attachment; filename=opinions.json"})
+    path = f"products/{productId}.json"
+    return send_file(path, as_attachment=True)
+    
 
 @app.route('/getXML/<productId>')
 def getXML(productId):
     with open(f"app/products/{productId}.json", "r", encoding="UTF-8") as f:
         producto = json.load(f)
-
     r = e.Element("Opinions")
     e.SubElement(r,"productId").text = producto["productId"]
     e.SubElement(r,"productName").text = producto["productName"]
-    project = e.SubElement(r,"Opinions")
+    kazda = e.SubElement(r,"Opinions")
     for z in producto["opinions"]:
+        project = e.SubElement(kazda,"opinia")
         e.SubElement(project,"opinionId").text = z["opinionId"]
         e.SubElement(project,"author").text = z["author"]
         e.SubElement(project,"rcmd").text = str(z["rcmd"])
@@ -117,8 +108,15 @@ def getXML(productId):
         e.SubElement(project,"purchaseDate").text = str(z["purchaseDate"])
         e.SubElement(project,"useful").text = str(z["useful"])
         e.SubElement(project,"useless").text = str(z["useless"])
-    a = e.ElementTree(r)
+        a = e.ElementTree(r)
     a.write("opinions.xml")
-    path = "C:\\Users\\domin\\Desktop\\Studia\\2SEM\prac_prog2\\CeneoWebScraper\\opinions.xml"
+    path = "static/opinions.xml"
     return send_file(path, as_attachment=True)
-    #return Response(, mimetype="text/xml", headers={"Content-disposition":"attachment; filename=opinions.xml"})
+    
+
+@app.route('/product/<productId>/wykresy')
+def wykresy(productId):
+    product = Product(productId)
+    product.importProduct()
+
+    return render_template('wykresy.html.jinja')
